6c0d6f7167e11ebf8af463c336d298a28ef6399e
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2025-08-13 15:32:58 -0600

    Actualizacion de Backend de la API de Banca Virtual Sotecpro

---
+++ api-banca/app/Http/Controllers/CresoliController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CremcreMetum;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+
+class CresoliController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function listcredit()
+    {
+        // This method is intended to list credit-related data.
+        // Implementation will depend on the specific requirements.
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CremcreMetum $cremcreMetum)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CremcreMetum $cremcreMetum)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CremcreMetum $cremcreMetum)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CremcreMetum $cremcreMetum)
+    {
+        //
+    }
+}
--- api-banca/routes/api.php
+++ api-banca/routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CresoliController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
+//rutas Iniciales para MSplusw Virtual Banca
+Route::post('creditos/solicitudes', [CresoliController::class, 'store']);
 
 
 
