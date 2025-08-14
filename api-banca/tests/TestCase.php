d72979e8de5dbd30200f01d39bfc5bfc1f91c0d8
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2025-08-14 10:19:31 -0600

    feat: add client code login endpoint

--- api-banca/tests/TestCase.php
+++ api-banca/tests/TestCase.php
@@ -4,7 +4,10 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\CreatesApplication;
+
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication, RefreshDatabase;
 }
