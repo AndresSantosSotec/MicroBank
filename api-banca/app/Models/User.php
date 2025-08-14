122ec4e702cf1f05098b59b6aee0776042346413
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2025-08-14 15:23:59 -0600

    feat: secure routes and username login

--- api-banca/app/Models/User.php
+++ api-banca/app/Models/User.php
@@ -1,33 +1,58 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 /**
  * Class User
- * 
+ *
  * @property int $id
- * @property string $username
+ * @property string $name
+ * @property string $email
+ * @property \Carbon\Carbon|null $email_verified_at
  * @property string $password
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
  *
  * @package App\Models
  */
-class User extends Model
+class User extends Authenticatable
 {
-	protected $table = 'users';
-	public $timestamps = false;
+    use HasApiTokens, HasFactory, Notifiable;
 
-	protected $hidden = [
-		'password'
-	];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
-	protected $fillable = [
-		'username',
-		'password'
-	];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
+
--- api-banca/routes/api.php
+++ api-banca/routes/api.php
@@ -8,12 +8,11 @@ use App\Http\Controllers\CatalogosEconomicosController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-//rutas Iniciales para MSplusw Virtual Banca
-Route::post('creditos/solicitudes', [CresoliController::class, 'store']);
+// Secure routes for the backend
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('creditos/solicitudes', [CresoliController::class, 'store']);
 
-
-
-
-Route::get('/agencias', [AgenciasController::class, 'index']);
-Route::get('/economia/sectores', [CatalogosEconomicosController::class, 'sectores']);
-Route::get('/economia/sectores/{sectorId}/actividades', [CatalogosEconomicosController::class, 'actividades']);
+    Route::get('/agencias', [AgenciasController::class, 'index']);
+    Route::get('/economia/sectores', [CatalogosEconomicosController::class, 'sectores']);
+    Route::get('/economia/sectores/{sectorId}/actividades', [CatalogosEconomicosController::class, 'actividades']);
+});
